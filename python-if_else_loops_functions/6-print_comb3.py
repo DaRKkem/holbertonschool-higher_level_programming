c330db41c3bba6261d5fc2ba130c0029bb10e108
DaRKkem <damross446@gmail.com>
2026-01-14 01:41:52 +0100

    correction

--- python-if_else_loops_functions/6-print_comb3.py
+++ python-if_else_loops_functions/6-print_comb3.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-for i in range(0, 9): # 9 et pas 10 car 90 ou + jamais atteint (89 max, où i = 8)
+for i in range(0, 9):
     for j in range(i + 1, 10):
         if i == 8 and j == 9:
             print("{}{}".format(i, j))
