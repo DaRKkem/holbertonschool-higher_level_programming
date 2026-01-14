135e5301a00c218887c007387b9a893a90c4d36b
DaRKkem <damross446@gmail.com>
2026-01-14 01:31:00 +0100

    correction

--- python-if_else_loops_functions/12-fizzbuzz.py
+++ python-if_else_loops_functions/12-fizzbuzz.py
@@ -1,12 +1,12 @@
 #!/usr/bin/python3
 def fizzbuzz():
-	""" Print the FizzBuzz """
-	for i in range(1, 101):
-		if i % 3 == 0 and i % 5 == 0:
-			print("FizzBuzz ", end="")
-		elif i % 3 == 0:
-			print("Fizz ", end="")
-		elif i % 5 == 0:
-			print("Buzz ", end="")
-		else:
-			print("{} ".format(i), end="")
+    """ Print the FizzBuzz """
+    for i in range(1, 101):
+        if i % 3 == 0 and i % 5 == 0:
+            print("FizzBuzz ", end="")
+        elif i % 3 == 0:
+            print("Fizz ", end="")
+        elif i % 5 == 0:
+            print("Buzz ", end="")
+        else:
+            print("{} ".format(i), end="")
