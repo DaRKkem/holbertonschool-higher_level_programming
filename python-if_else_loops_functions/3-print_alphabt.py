2eeabea82a4e0a6ca6e3b82d8465d77614469c3c
DaRKkem <damross446@gmail.com>
2026-01-14 01:27:22 +0100

    correction

--- python-if_else_loops_functions/3-print_alphabt.py
+++ python-if_else_loops_functions/3-print_alphabt.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 a = ord('a')
 for i in range(a, (a+26)):
-    if chr(i) != 'q' and chr(i) != 'e':
-        print("{}".format(chr(i)), end="")
+	if chr(i) != 'q' and chr(i) != 'e':
+		print("{}".format(chr(i)), end="")
