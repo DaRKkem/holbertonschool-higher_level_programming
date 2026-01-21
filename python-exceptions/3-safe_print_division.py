698193250f7e2f423a648607cc8c5f3f90ba547f
DaRKkem <damross446@gmail.com>
2026-01-22 00:34:55 +0100

    exo_3_finished

--- python-exceptions/3-safe_print_division.py
+++ python-exceptions/3-safe_print_division.py
@@ -1,1 +1,11 @@
 #!/usr/bin/python3
+def safe_print_division(a, b):
+    """Divides two integers safely and prints the result in finally."""
+    result = None
+    try:
+        result = a / b
+    except ZeroDivisionError:
+        result = None
+    finally:
+        print("Inside result: {}".format(result))
+    return result
