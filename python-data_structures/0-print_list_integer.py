f6a4c4593776cc6ce99523261658233642434ae9
DaRKkem <damross446@gmail.com>
2026-01-21 11:30:07 +0100

    fixed

--- python-data_structures/0-print_list_integer.py
+++ python-data_structures/0-print_list_integer.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def print_list_integer(my_list=[]):
-	if my_list is None:
-		return None
-	for integer in my_list:
-		print("{:d}".format(integer))
+    if my_list is None:
+        return None
+    for integer in my_list:
+        print("{:d}".format(integer))
--- python-data_structures/2-replace_in_list.py
+++ python-data_structures/2-replace_in_list.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python3
 def replace_in_list(my_list, idx, element):
-	if my_list is None:
-		return None
-	if idx < 0:
-		return my_list
-	elif idx > len(my_list):
-		return None
-	else:
-		my_list[idx] = element
-		return my_list
+    if my_list is None:
+        return None
+    if idx < 0:
+        return my_list
+    elif idx > len(my_list):
+         return None
+    else:
+        my_list[idx] = element
+    return my_list
