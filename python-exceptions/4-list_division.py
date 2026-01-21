31ff97ad88da05a5ee1e6acf8844aad002e567bf
DaRKkem <damross446@gmail.com>
2026-01-22 00:46:23 +0100

    exo_4_finished

--- python-exceptions/4-list_division.py
+++ python-exceptions/4-list_division.py
@@ -1,1 +1,18 @@
 #!/usr/bin/python3
+def list_division(my_list_1, my_list_2, list_length):
+    """Divide element by element two lists safely and return a new list."""
+    result = []
+    for i in range(list_length):
+        res = 0
+        try:
+            try:
+                res = my_list_1[i] / my_list_2[i]
+            except ZeroDivisionError:
+                print("division by 0")
+            except TypeError:
+                print("wrong type")
+        except IndexError:
+            print("out of range")
+        finally:
+            result.append(res)
+    return result
