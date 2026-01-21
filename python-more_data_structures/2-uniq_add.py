f20bf53b03d09e2f56d7a9c4c8b2b9844b734c4f
DaRKkem <damross446@gmail.com>
2026-01-21 11:25:17 +0100

    fixed

--- python-more_data_structures/2-uniq_add.py
+++ python-more_data_structures/2-uniq_add.py
@@ -1,1 +1,13 @@
 #!/usr/bin/python3
+def uniq_add(my_list=[]):
+    result = 0
+    copy = []
+    for element in my_list:
+        copy.append(element)
+        result += element
+        print(result)
+    return result
+
+my_list = [1, 2, 3, 1, 4, 2, 5]
+result = uniq_add(my_list)
+print("Result: {:d}".format(result))
