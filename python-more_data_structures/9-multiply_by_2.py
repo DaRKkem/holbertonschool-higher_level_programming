3bddf6c2036a09288da4e0ad14987ce83955c1fa
DaRKkem <damross446@gmail.com>
2026-01-21 16:11:35 +0100

    exo_9_finished

--- python-more_data_structures/9-multiply_by_2.py
+++ python-more_data_structures/9-multiply_by_2.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def multiply_by_2(a_dictionary):
-    new_dictionnary = {}
+    new_dictionary = {}
     for key in a_dictionary:
         new_dictionary[key] = a_dictionary[key] * 2
     return new_dictionary
