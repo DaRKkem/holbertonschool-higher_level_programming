ab9fc02be867cdf21115bbc0e6971fec05d6814b
DaRKkem <damross446@gmail.com>
2026-01-21 19:58:40 +0100

    exo_12_finished

--- python-more_data_structures/12-roman_to_int.py
+++ python-more_data_structures/12-roman_to_int.py
@@ -1,1 +1,14 @@
 #!/usr/bin/python3
+def roman_to_int(roman_string):
+    if not isinstance(roman_string, str):
+        return 0
+    tab = {'M': 1000, 'D': 500, 'C': 100, 'L': 50, 'X': 10, 'V': 5, 'I': 1}
+    result = 0
+    length = len(roman_string)
+    for i in range(length):
+        value = tab[roman_string[i]]
+        if i + 1 < length and tab[roman_string[i+1]] > value:
+            result -= value
+        else:
+            result += value
+    return result
