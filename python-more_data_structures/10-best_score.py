575ba846865635f0c12bb6656d1ee9440387755e
DaRKkem <damross446@gmail.com>
2026-01-21 16:57:16 +0100

    exo_10_finished

--- python-more_data_structures/10-best_score.py
+++ python-more_data_structures/10-best_score.py
@@ -1,1 +1,9 @@
 #!/usr/bin/python3
+def best_score(a_dictionary):
+    if not a_dictionary:
+        return None
+    best = next(iter(a_dictionary))
+    for key in a_dictionary:
+        if a_dictionary[key] > a_dictionary[best]:
+            best = key
+    return best
