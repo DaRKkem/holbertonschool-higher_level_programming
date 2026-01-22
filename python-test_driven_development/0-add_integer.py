a43d8351ed0042474606cbf338b0795073da0df5
DaRKkem <damross446@gmail.com>
2026-01-22 10:22:08 +0100

    fixed_5

--- python-test_driven_development/0-add_integer.py
+++ python-test_driven_development/0-add_integer.py
@@ -5,11 +5,12 @@
 def add_integer(a, b=98):
     """Add two integers."""
 
-    if type(a) not in (int, float):
+    if type(a) not in (int, float) or type(a) is bool:
         raise TypeError("a must be an integer")
 
-    if type(b) not in (int, float):
+    if type(b) not in (int, float) or type(b) is bool:
         raise TypeError("b must be an integer")
+
 
     a = int(a)
     b = int(b)
