2d9a2adeffd987ae59ea652c8e698b06e12d987b
DaRKkem <damross446@gmail.com>
2026-01-30 11:38:30 +0100

    commit

--- python-more_classes/2-rectangle.py
+++ python-more_classes/2-rectangle.py
@@ -1,1 +1,47 @@
 #!/usr/bin/python3
+#!/usr/bin/python3
+"""This module defines a Rectangle class."""
+
+
+class Rectangle:
+    """A class that defines a rectangle by its width and height."""
+
+    def __init__(self, width=0, height=0):
+        """Initialize rectangle with optional width and height"""
+        self.width = width
+        self.height = height
+
+    @property
+    def width(self):
+        """Retrieve width"""
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        """Set width with validation"""
+        if not isinstance(value, int):
+            raise TypeError("width must be an integer")
+        if value < 0:
+            raise ValueError("width must be >= 0")
+        self.__width = value
+
+    @property
+    def height(self):
+        """Retrieve height"""
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        """Set height with validation"""
+        if not isinstance(value, int):
+            raise TypeError("height must be an integer")
+        if value < 0:
+            raise ValueError("height must be >= 0")
+        self.__height = value
+
+    def area(self):
+        return self.width * self.height
+
+    def perimeter(self):
+        if self.width > 0 and self.height > 0:
+            return self.width * 2 + self . height * 2
