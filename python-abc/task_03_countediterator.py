93e470d80d00eef49e1a32453e2ec77d3d6eebea
DaRKkem <damross446@gmail.com>
2026-02-04 17:57:34 +0100

    commit

--- python-abc/task_03_countediterator.py
+++ python-abc/task_03_countediterator.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-"""Module defining CountedIterator that tracks iteration count."""
+"""This module defines a CountedIterator class that wraps an iterable"""
 
 
 class CountedIterator:
