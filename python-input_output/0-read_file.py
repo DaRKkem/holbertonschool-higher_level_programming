48188b572080784c6a1c5cb98175fd6762cf6baa
DaRKkem <damross446@gmail.com>
2026-02-09 12:04:16 +0100

    commit

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 
 def read_file(filename=""):
-    with open(filename, "r", encoding="utf-8") as f:
+    with open(filename, encoding="utf-8") as f:
         print(f.read(), end="")
 
 read_file("my_file_0.txt")
