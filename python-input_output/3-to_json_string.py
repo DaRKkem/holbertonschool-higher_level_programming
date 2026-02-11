fc568eca4afefbd81929edc86f870c99f1ac10fd
DaRKkem <damross446@gmail.com>
2026-02-11 11:30:52 +0100

    commit

--- python-input_output/3-to_json_string.py
+++ python-input_output/3-to_json_string.py
@@ -1,5 +1,9 @@
 #!/usr/bin/python3
+"""This module defines a function that returns
+   the JSON representation of an object (string)"""
+
 
 def to_json_string(my_obj):
+    """Returns the JSON representation of an object (string)"""
     with open(filename, "a", encoding="utf-8") as f:
         return f.write(text)
