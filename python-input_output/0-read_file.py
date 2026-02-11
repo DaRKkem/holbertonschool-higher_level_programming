b74612812e393a31839df1ad171988b5d494cb5f
DaRKkem <damross446@gmail.com>
2026-02-11 11:26:38 +0100

    commit

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -1,7 +1,10 @@
 #!/usr/bin/python3
+"""This module defines a function that reads a text file and prints it"""
+
+
 def read_file(filename=""):
     """"Reads a text file (UTF8) and prints it to stdout.
-    
+
     Args:
         filename (str): The name of the file to read.
     """
