9233c2a8099572efdf5eb544e1c66639740f678b
DaRKkem <damross446@gmail.com>
2026-02-11 11:27:23 +0100

    commit

--- python-input_output/2-append_write.py
+++ python-input_output/2-append_write.py
@@ -1,4 +1,6 @@
 #!/usr/bin/python3
+"""This module defines a function that appends a string to a text file"""
+
 
 def append_write(filename="", text=""):
     with open(filename, "a", encoding="utf-8") as f:
