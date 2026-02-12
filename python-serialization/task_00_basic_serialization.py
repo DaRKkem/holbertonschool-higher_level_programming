a772a3404e094ef17150788631198b2ec2d112f2
DaRKkem <damross446@gmail.com>
2026-02-12 15:09:59 +0100

    commit

--- python-serialization/task_00_basic_serialization.py
+++ python-serialization/task_00_basic_serialization.py
@@ -2,9 +2,6 @@
 """This module defines a function to serialize an object to a file."""
 
 
-from fileinput import filename
-
-
 def serialize_and_save_to_file(data, filename):
     """Serialize an object to a file in JSON format."""
     import json
@@ -16,3 +13,22 @@ def load_and_deserialize(filename):
     import json
     with open(filename, 'r')as f:
         return json.load(f)
+
+data_to_serialize = {
+    "name": "John Doe",
+    "age": 30,
+    "city": "New York"
+}
+
+# Serialize the data to JSON and save it to a file
+serialize_and_save_to_file(data_to_serialize, 'data.json')
+
+# Output: The data has been serialized and saved to 'data.json'
+print("Data serialized and saved to 'data.json'.")
+
+# Load and deserialize data from 'data.json'
+deserialized_data = load_and_deserialize('data.json')
+
+# Output: The deserialized data
+print("Deserialized Data:")
+print(deserialized_data)
