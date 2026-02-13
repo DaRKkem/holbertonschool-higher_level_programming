a20682784a9ce41285053c35d8fa8bc77dd0bc02
DaRKkem <damross446@gmail.com>
2026-02-13 10:08:12 +0100

    commit

--- python-serialization/task_01_pickle.py
+++ python-serialization/task_01_pickle.py
@@ -1,31 +1,38 @@
-#!/usr/bin/python3
-"""This module defines a CustomObject class with serialization."""
+#!/usr/bin/env python3
+"""Module for pickling custom classes."""
+import pickle
 
 
 class CustomObject:
-    """Defines a custom object."""
+    """CustomObject class with serialization capabilities."""
 
     def __init__(self, name, age, is_student):
-        if not isinstance(name, str):
-            raise TypeError("Wrong type for name")
-        if not isinstance(age, int):
-            raise TypeError("Wrong type for age")
-        if not isinstance(is_student, bool):
-            raise TypeError("Wrong type for is_student")
-
         self.name = name
         self.age = age
         self.is_student = is_student
 
-    def serialize(self, filename):
-        """Serialize the object using pickle."""
-        import pickle
-        with open(filename, "wb") as f:
-            pickle.dump(self, f)
+    def display(self):
+        """Display object attributes."""
+        print(f"Name: {self.name}")
+        print(f"Age: {self.age}")
+        print(f"Is Student: {self.is_student}")
 
-    @staticmethod
-    def deserialize(filename):
-        """Deserialize a file into a CustomObject."""
-        import pickle
-        with open(filename, "rb") as f:
-            return pickle.load(f)
+    def serialize(self, filename):
+        """Serialize the current instance to a file using pickle."""
+        try:
+            with open(filename, "wb") as f:
+                pickle.dump(self, f)
+        except (OSError, pickle.PickleError):
+            return None
+
+    @classmethod
+    def deserialize(cls, filename):
+        """Deserialize a file into a CustomObject instance."""
+        try:
+            with open(filename, "rb") as f:
+                obj = pickle.load(f)
+                if isinstance(obj, cls):
+                    return obj
+                return None
+        except (OSError, pickle.UnpicklingError, EOFError):
+            return None
