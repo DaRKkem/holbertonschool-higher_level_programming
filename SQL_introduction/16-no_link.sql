5db9fc8040fafec7d32483d646f75801845f207f
DaRKkem <damross446@gmail.com>
2026-03-05 15:44:08 +0100

    finished*

--- SQL_introduction/16-no_link.sql
+++ SQL_introduction/16-no_link.sql
@@ -1,3 +1,5 @@
+-- Display the score and name of records where the name
+-- is not NULL or empty, ordered by score descending
 SELECT score, name
 FROM second_table
 WHERE name IS NOT NULL AND name != ''
